508b8a31cbe76419dcdf49bd43e2395b3dd8def7
jisu1 <77786557+jisu1@users.noreply.github.com>
2021-03-28 22:59:51 +0800

    Add files via upload

--- animal.py
+++ animal.py
@@ -1,5 +1,5 @@
 # Author: Suge ji
 # Date: 28/03/2021
 # Description:Asks the user for their favorite animal and then prints out that that is their favorite animal
-fave_animal = input ("your favorite animal")
-print ("your favorite animal" +"fave_animal.")
+fave_animal = input("your favorite animal:")
+print("your favorite animal" + fave_animal + ".")
